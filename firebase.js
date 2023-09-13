90b12bed03c4195aa8e47623808ca771cb5ee8da
Abhay <abhaysharma.developer@gmail.com>
2023-09-14 00:46:46 +0530

    initial commit

---
+++ firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB3mk0gpKHXBQ0qant4ndHrttZvQowUmXI",
+  authDomain: "linkedin-a29a5.firebaseapp.com",
+  projectId: "linkedin-a29a5",
+  storageBucket: "linkedin-a29a5.appspot.com",
+  messagingSenderId: "538065251542",
+  appId: "1:538065251542:web:72b0b0110dc18bd9dfad77",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
